939f67ef68dfd81e2c3ba145538e151c3fc40429
sosagi2578@proexbol.com <sosagi2578@proexbol.com>
2023-03-16 06:39:34 +0000

    commit

--- models/src_reviews.sql
+++ models/src_reviews.sql
@@ -1,14 +1,13 @@
-WITH raw_reviews AS (
- SELECT
- *
- FROM
- RAW.PUBLIC.RAW_REVIEWS
+with raw_reviews as 
+(
+    select * from raw.public.raw_reviews
+    
 )
-SELECT
- listing_id,
- date AS review_date,
- reviewer_name,
- comments AS review_text,
- sentiment AS review_sentiment
-FROM
- raw_reviews
+
+select
+    listing_id,
+    date as review_date,
+    reviewer_name,
+    comments as review_text,
+    sentiment as review_sentiment
+from raw_reviews
